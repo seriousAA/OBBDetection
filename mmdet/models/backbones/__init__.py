1f4cf564382cc3bd7fd59544e2bf30f248121058
liyuqiu <gtawessd@gmail.com>
2024-01-19 19:18:19 +0800

    0119

--- mmdet/models/backbones/__init__.py
+++ mmdet/models/backbones/__init__.py
@@ -7,8 +7,14 @@ from .res2net import Res2Net
 from .resnet import ResNet, ResNetV1d
 from .resnext import ResNeXt
 from .ssd_vgg import SSDVGG
+from .vit_win_rvsa_wsz7 import ViT_Win_RVSA_V3_WSZ7
+from .vit_win_rvsa_kvdiff_wsz7 import ViT_Win_RVSA_V3_KVDIFF_WSZ7
+from .vitae_nc_win_rvsa_wsz7 import ViTAE_NC_Win_RVSA_V3_WSZ7
+from .vitae_nc_win_rvsa_kvdiff_wsz7 import ViTAE_NC_Win_RVSA_V3_KVDIFF_WSZ7
 
 __all__ = [
     'RegNet', 'ResNet', 'ResNetV1d', 'ResNeXt', 'SSDVGG', 'HRNet', 'Res2Net',
-    'HourglassNet', 'DetectoRS_ResNet', 'DetectoRS_ResNeXt'
+    'HourglassNet', 'DetectoRS_ResNet', 'DetectoRS_ResNeXt',
+    'ViT_Win_RVSA_V3_WSZ7', 'ViT_Win_RVSA_V3_KVDIFF_WSZ7',
+    'ViTAE_NC_Win_RVSA_V3_WSZ7', 'ViTAE_NC_Win_RVSA_V3_KVDIFF_WSZ7',
 ]
